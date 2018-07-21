1800b47b6257994ee97f1fbb17fae5aee3e0967c
cristianmmuresan <cristian@wearewip.com>
2018-07-21 09:44:42 +0300

    application class

---
+++ app/src/main/java/com/cristianmmuresan/traveltransylvania/TravelTransylvaniaApplication.java
@@ -0,0 +1,20 @@
+package com.cristianmmuresan.traveltransylvania;
+
+import android.app.Application;
+
+import com.cristianmmuresan.traveltransylvania.datacomposer.DataComposer;
+import com.cristianmmuresan.traveltransylvania.datacomposer.DataComposerPersister;
+
+public class TravelTransylvaniaApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Create database on first run
+        DataComposerPersister dataComposerPersister = new DataComposerPersister(getApplicationContext());
+        if (dataComposerPersister.getIsFirstRun()) {
+            DataComposer dataComposer = new DataComposer(getApplicationContext());
+            dataComposer.loadDataIntoDatabase();
+        }
+    }
+}
